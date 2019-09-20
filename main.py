f46a9278bbef1a734459ba476a598ce444182683
weavepub <50898384+weavepub@users.noreply.github.com>
2019-09-20 17:42:18 +0800

    排除添加某部门的用户

--- main.py
+++ main.py
@@ -29,7 +29,17 @@ class Main:
                 if k_info == 'id':
                     if v_info == gid:
                         return info
-                        #print(info)
+
+    def get_wechat_ugid(self, uid):
+        #根据uid获取企业微信的部门id,用于排除添加某部门用户
+        wechat_user_list = self.wechat.get_user_list()
+        for info in wechat_user_list:
+            for k_info, v_info in info.items():
+                if k_info == 'userid':
+                    v = v_info.lower()
+                    if v == uid:
+                        dep_id = info.get('department')
+                        return dep_id
 
     def add_user(self):
         ldap_uid = self.openldap.get_ldap_uid()
@@ -53,18 +63,23 @@ class Main:
 
         #添加用户
         for w_uid in wechat_uid:
-            #判断微信账号是否已经存在ldap中
+            # 判断微信账号是否已经存在ldap中
             if w_uid not in ldap_uid:
-                #列表[uid]转成字符串'uid'
+                # 不存在则向ldap添加账号信息
+                # 列表[uid]转成字符串'uid'
                 uid = "".join(w_uid)
-                wechat_uid_info = self.get_wechat_uid_info(uid)
-                #不存在则向ldap添加账号信息
-                def f(userid, name, mobile, email, position, department):
-                    print('开始添加ldap用户:%s' % userid)
-                    if self.openldap.ldap_add_user(userid, name, mobile, email, position, department):
-                        print('开始发送邮件')
-                        self.e_mail.send_mail(email, userid, name)
-                f(**wechat_uid_info)
+                #判断用户是否属于排除添加的部门:合作伙伴(:40)
+                exclude = [40]
+                wechat_dep_id = self.get_wechat_ugid(uid)
+                if wechat_dep_id not in exclude:
+                    #添加用户
+                    wechat_uid_info = self.get_wechat_uid_info(uid)
+                    def f(userid, name, mobile, email, position, department):
+                        print('开始添加ldap用户:%s' % userid)
+                        if self.openldap.ldap_add_user(userid, name, mobile, email, position, department):
+                            print('开始发送邮件')
+                            self.e_mail.send_mail(email, userid, name)
+                    f(**wechat_uid_info)
 
 
 if __name__ == "__main__":
